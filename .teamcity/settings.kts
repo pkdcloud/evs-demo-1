749574b2da1ec3eedec799aa2c9317479e2154cd
Paul Devine <paul@pkd.cloud>
2021-03-12 15:15:35 +1100

    enabled pipelines

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,55 +1,52 @@
-// package evsdemo1.buildTypes
+package evsdemo1.buildTypes
 
-// import jetbrains.buildServer.configs.kotlin.v2019_2.*
-// import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
-// object EvsDemo1_Build : BuildType({
-//     templates(evsdemo1_TfBuildRun)
-//     id = AbsoluteId("EvsDemo1_Build")
-//     name = "Build & Deploy"
-//     steps {
-//         script {
-//             name = "lint"
-//             id = "RUNNER_3"
-//             scriptContent = """
-//                 ls -la
-//                 echo "[LINT] Linting Project"
-//             """.trimIndent()
-//         }
-//         script {
-//             name = "static-appliaction-security-testing"
-//             id = "RUNNER_4"
-//             scriptContent = """
-//                 ls -la
-//                 echo "[SAST] Static application security testing"
-//             """.trimIndent()
-//         }
-//         script {
-//             name = "terraform-plan"
-//             id = "RUNNER_1"
-//             scriptContent = """
-//                 ls -la
-//                 TERRAFORM_ROOT_MODULE=horizon TERRAFORM_WORKSPACE=mgmt-apse2-horizon-collection make planAuto
-//             """.trimIndent()
-//         }
-//         script {
-//             name = "terraform-apply"
-//             id = "RUNNER_2"
-//             scriptContent = "TERRAFORM_ROOT_MODULE=horizon TERRAFORM_WORKSPACE=mgmt-apse2-horizon-collection make applyAuto"
-//         }
-//         script {
-//             name = "dynamic-application-code-testing"
-//             id = "RUNNER_6"
-//             scriptContent = """echo "[DAST] Dynamic application security testing""""
-//         }
-//         script {
-//             name = "function-testing"
-//             id = "RUNNER_5"
-//             scriptContent = """echo "[TEST] Functional Testing""""
-//         }
-//         stepsOrder = arrayListOf("RUNNER_3", "RUNNER_4", "RUNNER_1", "RUNNER_2", "RUNNER_6", "RUNNER_5")
-//     }
-//     requirements {
-//         matches("teamcity.agent.jvm.os.family", "Linux", "RQ_1")
-//     }
-// })
+object EvsDemo1_Build : BuildType({
+    templates(evsdemo1_TfBuildRun)
+    id = AbsoluteId("EvsDemo1_Build")
+    name = "Build & Deploy"
+    steps {
+        script {
+            name = "lint"
+            id = "RUNNER_3"
+            scriptContent = """
+                echo "[LINT] Linting Project"
+            """.trimIndent()
+        }
+        script {
+            name = "static-appliaction-security-testing"
+            id = "RUNNER_4"
+            scriptContent = """
+                echo "[SAST] Static application security testing"
+            """.trimIndent()
+        }
+        script {
+            name = "terraform-plan"
+            id = "RUNNER_1"
+            scriptContent = """
+                TERRAFORM_ROOT_MODULE=horizon TERRAFORM_WORKSPACE=mgmt-apse2-horizon-collection make planAuto
+            """.trimIndent()
+        }
+        script {
+            name = "terraform-apply"
+            id = "RUNNER_2"
+            scriptContent = "TERRAFORM_ROOT_MODULE=horizon TERRAFORM_WORKSPACE=mgmt-apse2-horizon-collection make applyAuto"
+        }
+        script {
+            name = "dynamic-application-code-testing"
+            id = "RUNNER_6"
+            scriptContent = """echo "[DAST] Dynamic application security testing""""
+        }
+        script {
+            name = "function-testing"
+            id = "RUNNER_5"
+            scriptContent = """echo "[TEST] Functional Testing""""
+        }
+        stepsOrder = arrayListOf("RUNNER_3", "RUNNER_4", "RUNNER_1", "RUNNER_2", "RUNNER_6", "RUNNER_5")
+    }
+    requirements {
+        matches("teamcity.agent.jvm.os.family", "Linux", "RQ_1")
+    }
+})
